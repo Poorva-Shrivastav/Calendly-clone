a6230cb9eda6b7089e05e86c77f8b8750302b802
Poorva-Shrivastav <35454765+Poorva-Shrivastav@users.noreply.github.com>
2024-04-26 15:02:21 +0530

    started firebase

--- frontend/calendly-clone/src/components/GoogleAccount/GoogleAccount.js
+++ frontend/calendly-clone/src/components/GoogleAccount/GoogleAccount.js
@@ -1,47 +1,54 @@
-import React, { useState } from 'react'
-import { GoogleLogin} from 'react-google-login'
-import './GoogleAccount.css'
-import axios from 'axios'
+import React, { useState } from "react";
+import { GoogleLogin } from "react-google-login";
+import "./GoogleAccount.css";
+import axios from "axios";
 
-function GoogleAccount({children}) {
-    const [isSignedIn, setIsSignedIn] = useState(false)
+function GoogleAccount({ children }) {
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
-    const googleSuccess = (res) => {
-        // setlogOutButton(true)
-        const userData = {tokenId: res.tokenId}
-        // axios.post('http://localhost:8000/api/googlelogin', userData)
-        axios.post('https://calendly-clon.herokuapp.com/api/googlelogin', userData)        
-        .then((data) => {
-            let responseJson = data;
-            sessionStorage.setItem('userData', JSON.stringify(data))
-            window.location = `/user` 
-        })
-    }
+  const googleSuccess = (res) => {
+    // setlogOutButton(true)
+    const userData = { tokenId: res.tokenId };
+    // axios.post('http://localhost:8000/api/googlelogin', userData)
+    axios
+      .post("https://calendly-clon.herokuapp.com/api/googlelogin", userData)
+      .then((data) => {
+        let responseJson = data;
+        sessionStorage.setItem("userData", JSON.stringify(data));
+        window.location = `/user`;
+      });
+  };
 
-    const googleFailure = (error) => {
-        console.log(error);
-        console.log("Google login was unsuccessful. Try Again later");
-    }
+  const googleFailure = (error) => {
+    console.log(error);
+    console.log("Google login was unsuccessful. Try Again later");
+  };
 
-    return (
-        <>
-            <GoogleLogin
-                clientId= {process.env.REACT_APP_CLIENT_ID}
-                render={renderProps => (
-                    <button id="button-Goglesignin"onClick={renderProps.onClick} disabled={renderProps.disabled}>{children}</button>
-                )}
-                onSuccess={googleSuccess}
-                onFailure={googleFailure}
-                cookiePolicy={'single_host_origin'}
-                id="button-Goglesignin"
-             />
-            {/* <GoogleLogout
+  return (
+    <>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        render={(renderProps) => (
+          <button
+            id="button-Goglesignin"
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+          >
+            {children}
+          </button>
+        )}
+        onSuccess={googleSuccess}
+        onFailure={googleFailure}
+        cookiePolicy={"single_host_origin"}
+        id="button-Goglesignin"
+      />
+      {/* <GoogleLogout
                 clientId= {process.env.REACT_APP_CLIENT_ID}
                 buttonText="Logout"
                 onLogoutSuccess={logOutSuccess}
             /> */}
-        </>
-    )
+    </>
+  );
 }
 
-export default GoogleAccount
+export default GoogleAccount;
